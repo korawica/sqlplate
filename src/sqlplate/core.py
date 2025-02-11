cc2bea7da5eac3789faa1389de76bde82ff284c0
Korawich Anuttra <korawich.anu@gmail.com>
2025-02-11 21:38:08 +0700

    :toolbox: build: add header and remove token on publish workflow.

--- src/sqlplate/core.py
+++ src/sqlplate/core.py
@@ -1,3 +1,10 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2022 Korawich Anuttra. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for
+# license information.
+# ------------------------------------------------------------------------------
+from __future__ import annotations
+
 from pathlib import Path
 from typing import Any
 
