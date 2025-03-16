5d4389b6aea25c8e8d76c025d0d720142a69b56b
Korawich Anuttra <korawich.anu@gmail.com>
2025-03-16 22:20:15 +0700

    :recycle: clean: rename class method for list formats on template folder.

---
+++ src/sqlplate/sqlity.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2022 Korawich Anuttra. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for
+# license information.
+# ------------------------------------------------------------------------------
+from __future__ import annotations
+
+
+class SQLity:
+    """A SQLity object for render data quality report by Jinja template."""
--- tests/test_sqlplate.py
+++ tests/test_sqlplate.py
@@ -11,7 +11,7 @@ def test_sqlplate_raise(template_path):
 
 
 def test_sqlplate_formats(template_path):
-    formats: list[str] = SQLPlate.formats(path=template_path)
+    formats: list[str] = SQLPlate.list_formats(path=template_path)
     assert isinstance(formats, list)
     assert len(formats) > 0
     assert 'utils' not in formats
